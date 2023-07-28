c4c331d43eecc6ce9dce648726880a1f5c38e3f0
Shivam Bhatt <4u14shivu@gmail.com>
2023-07-28 14:42:43 -0400

    commited 28-7-2023

--- src/component/mainComponent/about.js
+++ src/component/mainComponent/about.js
@@ -2,7 +2,45 @@ import Past from '../images/past.jpg'
 import Present from '../images/present.jpg'
 import Future from '../images/future.jpg'
 import './about.css'
+
+// const [values, setValues] = useState(['Value 1', 'Value 2', 'Value 3']);
 const About = () => {
+  const about = [
+    {
+      i: Past,
+      h: "Unearthing the Past",
+      p: `6 months of Expirience as a SEO engineer in Plant Ayurveda ptv. ltd., Mohali (India) ${<br></br>} 1.6 years of Expirience as a Software developer in HSVP Panchkula Haryana (India). `
+    },
+    {
+      i: Present,
+      h: "Current Pursuits",
+      p: "currently i am working as a software developer in New Haryana Civil Secratriate, Chandigarh (India).<br></br>i am enjoying my work as a software developer.",
+    },
+    {
+      i: Future,
+      h: "Future Vision",
+      p: "I am a forward-thinking software developer with a vision to lead an innovative IT company that leverages cutting-edge technologies to reshape industries and drive positive change. Together, we will build transformative solutions that empower businesses and propel us into a future of boundless possibilities."
+    }
+  ]
+
+  const about1 = [
+    {
+      i: Past,
+      h: "Unearthing the Past",
+      p: `6 months of Expirience as a SEO engineer in Plant Ayurveda ptv. ltd., Mohali (India) and 1.6 years of Expirience as a Software developer in HSVP Panchkula Haryana (India). `
+    },
+    {
+      i: Present,
+      h: "Current Pursuits",
+      p: "currently i am working as a software developer in New Haryana Civil Secratriate, Chandigarh (India). i am enjoying my work as a software developer.",
+    },
+    {
+      i: Future,
+      h: "Future Vision",
+      p: "I am a forward-thinking software developer with a vision to lead an innovative IT company that leverages cutting-edge technologies to reshape industries and drive positive change. Together, we will build transformative solutions that empower businesses and propel us into a future of boundless possibilities."
+    }
+  ]
+
   return (
     <>
       <div className="card-body ">
@@ -18,14 +56,14 @@ const About = () => {
               </p>
               <h5 class="card-title  m-3">Skills</h5>
               <ul>
-              <li>
+                <li>
                   Strong knowledge of (HTML, CSS, JS, Bootstrap)
                 </li>
                 <li>
                   Dot net developer (asp.net, c#, MVC, Web API)
                 </li>
                 <li>
-                   Database Tools (SQL, postgresql )
+                  Database Tools (SQL, postgresql )
                 </li>
                 <li>
                   knowledge of (React, Node js, Express for Node API, Mongodb )
@@ -33,17 +71,17 @@ const About = () => {
               </ul>
               <h5 class="card-title  m-3">Expirience</h5>
               <p class="card-text">
-              I have total 1.5<sup>+</sup> years of Expirience. as follow:
+                I have total 1.5<sup>+</sup> years of Expirience. as follow:
               </p>
               <ul>
-              <li>
-              3 months of Expirience as a SEO engineer.
+                <li>
+                  3 months of Expirience as a SEO engineer.
                 </li>
                 <li>
-                1.7 years of Expirience as a Software developer.
+                  1.7 years of Expirience as a Software developer.
                 </li>
               </ul>
-              
+
             </div>
           </div>
           <div className='col-md-12 col-sm-12 col-lg-2 col-xl-2 col-xxl-2'>
@@ -51,30 +89,18 @@ const About = () => {
         </div>
         <div class="row g-0">
           <div class="col-md-12 col-sm-12 col-lg-1 col-xl-1 col-xxl-1"></div>
-          <div class="card col-md-12 col-sm-12 col-lg-3 col-xl-3 col-xxl-3 m-3 border border-light shadow-lg rounded card-hover">
-            <img src={Past} class="img-fluid rounded-end" alt="..." className='img-fluid' />
-            <div class="card-body ">
-              <h5 class="card-title">Unearthing the Past</h5>
-              <p class="card-text"> 3 months of Expirience as a SEO engineer in Plant Ayurveda ptv. ltd., Mohali (India) </p>
-              <p class="card-text"> 1.5 years of Expirience as a Software developer in HSVP Panchkula Haryana (India). </p>
-            </div>
-          </div>
-          
-          <div class="card col-md-12 col-sm-12 col-lg-3 col-xl-3 col-xxl-3 m-3 border border-light shadow-lg rounded card-hover">
-            <img src={Present} class="img-fluid rounded-end" alt="..." className='img-fluid ' />
-            <div class="card-body">
-              <h5 class="card-title">Current Pursuits</h5>
-              <p class="card-text">currently i am working as a software developer in New Haryana Civil Secratriate, Chandigarh (India).<br></br>i am enjoying my work as a software developer.
-              </p>
-            </div>
-          </div>
-          <div class="card col-md-12 col-sm-12 col-lg-3 col-xl-3 col-xxl-3 m-3 border border-light shadow-lg rounded card-hover">
-            <img src={Future} class="img-fluid rounded-end" alt="..." className='img-fluid' />
-            <div class="card-body">
-              <h5 class="card-title">Future Vision</h5>
-              <p class="card-text">I am a forward-thinking software developer with a vision to lead an innovative IT company that leverages cutting-edge technologies to reshape industries and drive positive change. Together, we will build transformative solutions that empower businesses and propel us into a future of boundless possibilities.</p>
-            </div>
-          </div>
+          {
+            about.map((info, index) => {
+              return (
+                <div class="card col-md-12 col-sm-12 col-lg-3 col-xl-3 col-xxl-3 m-3 border border-light shadow-lg rounded card-hover" key={index}>
+                  <img src={info.i} class="img-fluid rounded-end" alt="..." className='img-fluid' />
+                  <div class="card-body ">
+                    <h5 class="card-title">{info.h}</h5>
+                    <p class="card-text">{info.p}</p>
+                  </div>
+                </div>)
+            })
+          }
           <div class="col-md-12 col-sm-12 col-lg-1 col-xl-1 col-xxl-1"></div>
         </div>
       </div>
--- src/component/mainComponent/home.js
+++ src/component/mainComponent/home.js
@@ -3,31 +3,37 @@ import MyImage from '../images/home page.jpg'
 import { Download } from 'react-bootstrap-icons';
 const Home = () =>{
   const resumeUrl = '/Shivam-Resume.pdf';
+  const MyInfo ={
+    para1:"I'm Shivam Bhatt, a software developer with a passion for crafting innovative solutions and bringing ideas to life through code. With a strong foundation in web development and a keen eye for detail, I strive to create seamless and user-centric digital experiences.",
+    para2:"My journey in software development has been driven by a love for problem-solving and a desire to make a positive impact on the world. I thrive in collaborative environments, where I can work alongside talented teams to tackle complex challenges and turn them into elegant solutions.",
+    para3:" With expertise in various programming languages, frameworks, and technologies, I continuously seek to expand my knowledge and stay up-to-date with the latest industry trends. Whether it's building interactive web applications, designing responsive user interfaces, or optimizing backend functionality, I'm always eager to take on new and exciting projects.",
+    para4:" As a software developer, I believe in the power of clean and maintainable code, ensuring that every line I write contributes to robust and scalable solutions. I take pride in my ability to translate ideas into reality and deliver exceptional results that exceed expectations.",
+    para5:" I invite you to explore my portfolio, where you can find a collection of my projects that showcase my skills and dedication to the world of software development. I'm excited to collaborate on new ventures and contribute my expertise to making a difference in the digital landscape.",
+    para6:" Let's connect and discuss how I can help bring your vision to fruition. I look forward to embarking on new challenges and making a lasting impact as a software developer. Thank you for visiting my profile!"
+  }
+  const paragraphs = Object.values(MyInfo);
     return(
-      <>
-      <div className="card-body">
+        <>
+        <div className="card-body">
           <div class="row g-0">
             <div className='col-md-12 col-sm-12 col-lg-1 col-xl-1 col-xxl-1'></div>
-       <div class="card mt-5 border col-md-12 col-sm-12 col-lg-10 col-xl-10 col-xxl-10  shadow-lg rounded ">
-        <div className='row'>
-       <div class="card-body col-md-12 col-sm-12 col-lg-6 col-xl-6 col-xxl-6">
+            <div class="card mt-5 border col-md-12 col-sm-12 col-lg-10 col-xl-10 col-xxl-10  shadow-lg rounded ">
+              <div className='row'>
+                <div class="card-body col-md-12 col-sm-12 col-lg-6 col-xl-6 col-xxl-6">
                   <h5 class="card-title">Hello,</h5>
-                  <p class="card-text">I'm Shivam Bhatt, a software developer with a passion for crafting innovative solutions and bringing ideas to life through code. With a strong foundation in web development and a keen eye for detail, I strive to create seamless and user-centric digital experiences.</p>
-                  <p class="card-text">My journey in software development has been driven by a love for problem-solving and a desire to make a positive impact on the world. I thrive in collaborative environments, where I can work alongside talented teams to tackle complex challenges and turn them into elegant solutions.</p>
-                  <p class="card-text"> With expertise in various programming languages, frameworks, and technologies, I continuously seek to expand my knowledge and stay up-to-date with the latest industry trends. Whether it's building interactive web applications, designing responsive user interfaces, or optimizing backend functionality, I'm always eager to take on new and exciting projects.</p>
-                  <p class="card-text"> As a software developer, I believe in the power of clean and maintainable code, ensuring that every line I write contributes to robust and scalable solutions. I take pride in my ability to translate ideas into reality and deliver exceptional results that exceed expectations.</p>
-                  <p class="card-text"> I invite you to explore my portfolio, where you can find a collection of my projects that showcase my skills and dedication to the world of software development. I'm excited to collaborate on new ventures and contribute my expertise to making a difference in the digital landscape.</p>
-                  <p class="card-text"> Let's connect and discuss how I can help bring your vision to fruition. I look forward to embarking on new challenges and making a lasting impact as a software developer. Thank you for visiting my profile!</p>
-<a href={resumeUrl} download = 'Shivam-Resume.pdf' class="btn btn-dark">Download Resume <Download/></a>
+                  {paragraphs.map((paragraph, index) =>{
+                   return <p class='card-text' key={index} >{paragraph}</p>
+                  })}
+                  <a href={resumeUrl} download='Shivam-Resume.pdf' class="btn btn-dark">Download Resume <Download /></a>
                 </div>
-                <div class="col-md-12 col-sm-12 col-lg-4 col-xl-4col-xxl-4" style={{overflow:"hidden"}}>
-                <img src={MyImage} class="img-fluid rounded-end card-img-left" alt="..." style={{width:"100%",height:"100%", objectFit:"fill"}}/>
+                <div class="col-md-12 col-sm-12 col-lg-4 col-xl-4col-xxl-4" style={{ overflow: "hidden" }}>
+                  <img src={MyImage} class="img-fluid rounded-end card-img-left" alt="..." style={{ width: "100%", height: "100%", objectFit: "fill" }} />
                 </div>
-            </div>
+              </div>
             </div>
             <div className='col-md-12 col-sm-12 col-lg-1 col-xl-1 col-xxl-1'></div>
-            </div>
-            </div>
+          </div>
+        </div>
       </>
     )
 }
